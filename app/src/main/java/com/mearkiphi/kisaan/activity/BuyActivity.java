5fab3151e0c9d47a1292b51d09abb5bf5e96e0fd
nisarg <nisargnegi@gmail.com>
2017-05-13 17:05:08 +0530

    permissions added

--- app/src/main/java/com/mearkiphi/kisaan/activity/BuyActivity.java
+++ app/src/main/java/com/mearkiphi/kisaan/activity/BuyActivity.java
@@ -1,12 +1,16 @@
 package com.mearkiphi.kisaan.activity;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -34,6 +38,11 @@ public class BuyActivity extends AppCompatActivity {
     private TextView textViewRate, textViewSubType, textViewLocation;
     private FloatingActionButton fab, fab2;
     GPSTracker gps;
+    private static final int PERMISSION_CALLBACK_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
+    String[] permissionsRequired = new String[]{Manifest.permission.CALL_PHONE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION};
 
 
     @Override
@@ -42,7 +51,73 @@ public class BuyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item_detail);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setTitle(getIntent().getStringExtra("sub_category"));
+        checkPermissions();
         fetchTodosFromDB();
+    }
+
+    private void checkPermissions() {
+        {
+            if(ActivityCompat.checkSelfPermission(BuyActivity.this, permissionsRequired[0]) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(BuyActivity.this, permissionsRequired[1]) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(BuyActivity.this, permissionsRequired[2]) != PackageManager.PERMISSION_GRANTED){
+                if(ActivityCompat.shouldShowRequestPermissionRationale(BuyActivity.this,permissionsRequired[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(BuyActivity.this,permissionsRequired[1])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(BuyActivity.this,permissionsRequired[2])){
+                    //Show Information about why you need the permission
+                    AlertDialog.Builder builder = new AlertDialog.Builder(BuyActivity.this);
+                    builder.setTitle("Need Multiple Permissions");
+                    builder.setMessage("This app needs Camera and Location permissions.");
+                    builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            ActivityCompat.requestPermissions(BuyActivity.this,permissionsRequired,PERMISSION_CALLBACK_CONSTANT);
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    builder.show();
+                } else if (false) {
+                    //Previously Permission Request was cancelled with 'Dont Ask Again',
+                    // Redirect to Settings after showing Information about why you need the permission
+                    AlertDialog.Builder builder = new AlertDialog.Builder(BuyActivity.this);
+                    builder.setTitle("Need Multiple Permissions");
+                    builder.setMessage("This app needs Camera and Location permissions.");
+                    builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            //sentToSettings = true;
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            Uri uri = Uri.fromParts("package", getPackageName(), null);
+                            intent.setData(uri);
+                            startActivityForResult(intent, REQUEST_PERMISSION_SETTING);
+                            Toast.makeText(getBaseContext(), "Go to Permissions to Grant  Camera and Location", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    builder.show();
+                }  else {
+                    //just request the permission
+                    ActivityCompat.requestPermissions(BuyActivity.this,permissionsRequired,PERMISSION_CALLBACK_CONSTANT);
+                }
+
+                //txtPermissions.setText("Permissions Required");
+
+//                SharedPreferences.Editor editor = permissionStatus.edit();
+//                editor.putBoolean(permissionsRequired[0],true);
+//                editor.commit();
+            }
+        }
     }
 
     private void fetchTodosFromDB() {
@@ -131,4 +206,63 @@ public class BuyActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == PERMISSION_CALLBACK_CONSTANT){
+            //check if all permissions are granted
+            boolean allgranted = false;
+            for(int i=0;i<grantResults.length;i++){
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                    allgranted = true;
+                } else {
+                    allgranted = false;
+                    break;
+                }
+            }
+
+            if(allgranted){
+                proceedAfterPermission();
+            } else if(ActivityCompat.shouldShowRequestPermissionRationale(BuyActivity.this,permissionsRequired[0])
+                    || ActivityCompat.shouldShowRequestPermissionRationale(BuyActivity.this,permissionsRequired[1])
+                    || ActivityCompat.shouldShowRequestPermissionRationale(BuyActivity.this,permissionsRequired[2])){
+             //   txtPermissions.setText("Permissions Required");
+                AlertDialog.Builder builder = new AlertDialog.Builder(BuyActivity.this);
+                builder.setTitle("Need Multiple Permissions");
+                builder.setMessage("This app needs Camera and Location permissions.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        ActivityCompat.requestPermissions(BuyActivity.this,permissionsRequired,PERMISSION_CALLBACK_CONSTANT);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else {
+                Toast.makeText(getBaseContext(),"Unable to get Permission",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PERMISSION_SETTING) {
+            if (ActivityCompat.checkSelfPermission(BuyActivity.this, permissionsRequired[0]) == PackageManager.PERMISSION_GRANTED) {
+                //Got Permission
+                proceedAfterPermission();
+            }
+        }
+    }
+    private void proceedAfterPermission() {
+        //txtPermissions.setText("We've got all permissions");
+        Toast.makeText(getBaseContext(), "We got All Permissions", Toast.LENGTH_LONG).show();
+    }
+
 }
